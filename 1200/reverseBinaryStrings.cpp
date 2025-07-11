f3d70075226ea7baabc639298da5606d09dedd82
shubhdevv <shubhamdhoni250804@gmail.com>
2025-07-11 22:25:11 +0530

    day8

---
+++ 1200/reverseBinaryStrings.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+# define M_PI  3.14159265358979323846
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    int c = 0;
+    for (int i = 0; i < n - 1; ++i)
+        if (s[i] == s[i + 1]) ++c;
+
+    cout << (c + 1) / 2 << '\n';
+}
+
+    int main(){
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        cout<<fixed;
+        cout<<setprecision(10);
+//        freopen("timber_input.txt", "r", stdin);
+//        freopen("timber_output.txt", "w", stdout);
+         int t=1;
+         cin>>t;
+         for(int i=1;i<=t;i++){
+            solve();
+        }
+        return 0;
+    }  
