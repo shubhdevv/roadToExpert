30a31b19942ecf9b74bb0291704098affba9dffe
shubhdevv <shubhamdhoni250804@gmail.com>
2025-10-27 23:38:28 +0530

    1300

---
+++ 1300/negativePrefixes.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+# define M_PI  3.14159265358979323846
+void solve() {
+     int n;
+        cin>>n;
+        vector<int>a(n);
+        vector<int>b;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            if(x==0){
+                b.push_back(a[i]);
+                a[i]=(int)(1e6);
+            }
+        }
+        sort(b.begin(),b.end(),greater<>());
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==(int)(1e6)){
+                a[i]=b[j];
+                j++;
+            }
+        }
+        for(auto i : a)
+            cout<<i<<" ";
+        cout<<"\n";
+}
+
+    int main(){
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        cout<<fixed;
+        cout<<setprecision(10);
+//        freopen("timber_input.txt", "r", stdin);
+//        freopen("timber_output.txt", "w", stdout);
+         int t=1;
+         cin>>t;
+         for(int i=1;i<=t;i++){
+            solve();
+        }
+        return 0;
+    }  
