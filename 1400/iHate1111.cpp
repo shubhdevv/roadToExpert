9a84305a2d10547c86de1ab10ac355d99c4331f7
shubhdevv <shubhamdhoni250804@gmail.com>
2025-11-17 23:03:02 +0530

    1400

---
+++ 1400/iHate1111.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+# define M_PI  3.14159265358979323846
+void solve() {
+    ll x;
+    cin>>x;
+    ll r = x % 11;      
+    ll need = r * 111;  
+
+    if (x >= need) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+    int main(){
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        cout<<fixed;
+        cout<<setprecision(10);
+//        freopen("timber_input.txt", "r", stdin);
+//        freopen("timber_output.txt", "w", stdout);
+         int t=1;
+         cin>>t;
+         for(int i=1;i<=t;i++){
+            solve();
+        }
+        return 0;
+    }  
